3ca545f9fc0ed4ffbc4d4fe3ce8423e30970e6b4
Nilakshhh <dnilaksh05@gmail.com>
2024-02-16 12:30:24 +0530

    Basic login setup with jwt generation done

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+from models.employee import Employee
+import jwt
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+@app.route('/')
+def enter():
+    return render_template('login.html')
+
+@app.route('/login', methods = ["POST"])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    role = request.form.get('role')
+
+    if not email or not password:
+        return 'Missing email or password', 400
+
+    employee = Employee(email, password, role)
+    login_status = employee.authenticate(email, password, role)
+
+    if login_status:
+        # Create JWT token
+        token = jwt.encode({"email": email}, app.config['SECRET_KEY'], algorithm="HS256")
+
+        return jsonify({'token': token})
+
+    return jsonify({'message': 'Invalid email or password'}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/employee.py
@@ -0,0 +1,46 @@
+import psycopg2
+class Employee:
+    def __init__(self, email, password, role, created_at=None):
+        self.email = email
+        self.password = password
+        self.role = role
+        self.created_at = created_at
+    
+    
+    def authenticate(self, email, password, role):
+        if not self.validate():
+            return False
+        try:
+            # Connect to your PostgreSQL database
+            conn = psycopg2.connect(
+                dbname="emp",
+                user="postgres",
+                password="root",
+                host="localhost",
+                port="5432"
+            )
+
+            # Create a cursor object
+            cur = conn.cursor()
+
+            # Execute a SQL query to fetch user credentials
+            cur.execute("SELECT email, password, role FROM employee WHERE email = %s", (self.email,))
+
+            # Fetch the result
+            result = cur.fetchone()
+            cur.close()
+            conn.close()
+
+            # Check if user exists and password matches
+            if result and result[1] == self.password and result[2] == self.role:
+                return True
+            else:
+                return False
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL:", error)
+            return False
+
+    
+    def validate(self):
+        return True
